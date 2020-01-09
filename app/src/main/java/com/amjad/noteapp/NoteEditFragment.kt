dda3b792d87f9f87622282751f6a0eb3d0ccdb8b
Amjad Alsharafi <amjadsharafi10@gmail.com>
2020-01-09 16:47:02 +0800

    Add: note edit view (empty now)
    - responsible for viewing the full note and editing the note

---
+++ app/src/main/java/com/amjad/noteapp/NoteEditFragment.kt
@@ -0,0 +1,33 @@
+package com.amjad.noteapp.ui.fragments
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import com.amjad.noteapp.databinding.FragmentNoteEditBinding
+import com.amjad.noteapp.ui.viewmodels.NotesListViewModel
+
+class NoteEditFragment : Fragment() {
+
+    private lateinit var viewModel: NotesListViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = FragmentNoteEditBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(NotesListViewModel::class.java)
+        // TODO: Use the ViewModel
+        Log.i("BB", viewModel.notes.toString())
+    }
+
+}
--- app/src/main/java/com/amjad/noteapp/ui/adapters/NotesListAdapter.kt
+++ app/src/main/java/com/amjad/noteapp/ui/adapters/NotesListAdapter.kt
@@ -1,11 +1,13 @@
 package com.amjad.noteapp.ui.adapters
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.amjad.noteapp.R
 import com.amjad.noteapp.databinding.NoteitemViewBinding
 
 class NotesListAdapter : ListAdapter<String, NotesListAdapter.NoteViewHolder>(_NoteListDiffItemCallBack()) {
@@ -21,7 +23,12 @@ class NotesListAdapter : ListAdapter<String, NotesListAdapter.NoteViewHolder>(_N
     class NoteViewHolder (private val binding: NoteitemViewBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(text: String) {
             binding.apply {
+                Log.i("BB", text)
                 noteText.text = text
+                setOnNoteClick {
+                    //                    val direction =
+                    it.findNavController().navigate(R.id.action_mainFragment_to_noteEditFragment)
+                }
                 executePendingBindings()
             }
         }
