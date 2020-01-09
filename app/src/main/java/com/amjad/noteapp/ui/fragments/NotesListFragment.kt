27b7e02cc2996498d0a64cd620cefb13bbe8c661
Amjad Alsharafi <amjadsharafi10@gmail.com>
2020-01-09 16:45:57 +0800

    Edit: used livedata
    - with ability to add a new data from a FAB click

--- app/src/main/java/com/amjad/noteapp/ui/fragments/NotesListFragment.kt
+++ app/src/main/java/com/amjad/noteapp/ui/fragments/NotesListFragment.kt
@@ -1,11 +1,13 @@
 package com.amjad.noteapp.ui.fragments
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.amjad.noteapp.databinding.NoteslistFragmentBinding
 import com.amjad.noteapp.ui.adapters.NotesListAdapter
 import com.amjad.noteapp.ui.viewmodels.NotesListViewModel
@@ -13,19 +15,24 @@ import com.amjad.noteapp.ui.viewmodels.NotesListViewModel
 class NotesListFragment : Fragment() {
     private lateinit var viewModel: NotesListViewModel
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         val binding = NoteslistFragmentBinding.inflate(inflater, container, false)
-        val adapter =  NotesListAdapter()
+        val adapter = NotesListAdapter()
         binding.noteslistview.adapter = adapter
-
         observersInit(adapter)
+
+        binding.setOnNewNoteClick {
+            (viewModel.notes as MutableLiveData).setValue(viewModel.notes.value?.plus("again"))
+        }
 
         return binding.root
     }
 
     private fun observersInit(adapter: NotesListAdapter) {
-        adapter.submitList(viewModel.notes)
+        viewModel.notes.observe(this, Observer { notes -> adapter.submitList(notes) })
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/amjad/noteapp/ui/viewmodels/NotesListViewModel.kt
+++ app/src/main/java/com/amjad/noteapp/ui/viewmodels/NotesListViewModel.kt
@@ -1,9 +1,10 @@
 package com.amjad.noteapp.ui.viewmodels
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class NotesListViewModel : ViewModel() {
     // TODO: change to LiveData or something from web or DB
-    val notes : List<String> = listOf("welcome", "to")
+    val notes: LiveData<List<String>> = MutableLiveData<List<String>>(listOf("welcome", "there"))
 }
