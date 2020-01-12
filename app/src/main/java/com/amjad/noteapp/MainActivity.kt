00d32fa9d69c2fbfe007006ff7d56e923eabb278
Amjad Alsharafi <amjadsharafi10@gmail.com>
2020-01-12 23:27:58 +0800

    Add: back button on the edit fragment to get back to the list fragment
    - as navigator components work on fragments, this actions needed
    to be done sort of manually overriding the supportBar NavigationUp
    action

--- app/src/main/java/com/amjad/noteapp/MainActivity.kt
+++ app/src/main/java/com/amjad/noteapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.amjad.noteapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.amjad.noteapp.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +12,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         DataBindingUtil.setContentView<MainActivityBinding>(this, R.layout.main_activity)
+
+        val navController = findNavController(R.id.nav_host)
+        setupActionBarWithNavController(navController)
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host).navigateUp() ||
+                super.onSupportNavigateUp()
+    }
 }
