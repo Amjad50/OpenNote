0afcacf05907ba3bf2c7ef84ab569a8d7d10cbd2
Amjad Alsharafi <amjadsharafi10@gmail.com>
2020-01-15 09:51:10 +0800

    Edit: focus on noteEdit and show the keyboard on a new note
    - if a new note is created, auto focus on the noteEdit element
    for easier typing for the user.
    - also changed the -1L noteId into static const val

--- app/src/main/java/com/amjad/noteapp/ui/fragments/NoteEditFragment.kt
+++ app/src/main/java/com/amjad/noteapp/ui/fragments/NoteEditFragment.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.view.inputmethod.InputMethodManager.SHOW_IMPLICIT
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.navArgs
 import com.amjad.noteapp.data.Note
 import com.amjad.noteapp.databinding.NoteEditFragmentBinding
 import com.amjad.noteapp.ui.viewmodels.NoteViewModel
+
 
 class NoteEditFragment : Fragment() {
 
@@ -35,16 +39,28 @@ class NoteEditFragment : Fragment() {
 
         binding.model = viewModel
 
+        // start editing right away if this is a new note
+        if (args.noteId == NEW_NOTE_ID)
+            requestFocusAndShowKeyboard(binding.noteEdit)
+
         viewModel.setNoteID(args.noteId)
 
         return binding.root
+    }
+
+    private fun requestFocusAndShowKeyboard(noteEdit: View) {
+        if (noteEdit.requestFocusFromTouch())
+            context?.also {
+                val inputMethodManager = getSystemService(it, InputMethodManager::class.java)
+                inputMethodManager?.toggleSoftInput(SHOW_IMPLICIT, 0)
+            }
     }
 
     private fun saveNote() {
         val newNote = Note(binding.titleEdit.text.toString(), binding.noteEdit.text.toString())
 
         // no id sent mean that this is a new note
-        if (args.noteId != -1L) {
+        if (args.noteId != NEW_NOTE_ID) {
             newNote.id = args.noteId
             viewModel.updateNote(newNote)
         } else {
@@ -56,6 +72,15 @@ class NoteEditFragment : Fragment() {
     override fun onStop() {
         super.onStop()
         saveNote()
+
+        // hide the keyboard
+        context?.also {
+            val inputMethodManager = getSystemService(it, InputMethodManager::class.java)
+            inputMethodManager?.hideSoftInputFromWindow(binding.root.windowToken, 0)
+        }
     }
 
+    companion object {
+        const val NEW_NOTE_ID = -1L
+    }
 }
