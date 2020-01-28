ff396f497687fbebc6a45967787b2778fb723042
Amjad Alsharafi <amjadsharafi10@gmail.com>
2020-01-29 01:09:59 +0800

    Test: Added test for inserting new CheckableListNote
    - also changed some code for better quality
    - added another test that tests normal and list note together

--- app/src/androidTest/java/com/amjad/opennote/RoomDBTest.kt
+++ app/src/androidTest/java/com/amjad/opennote/RoomDBTest.kt
@@ -11,11 +11,13 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.amjad.opennote.data.daos.NoteDao
 import com.amjad.opennote.data.databases.NoteDatabase
+import com.amjad.opennote.data.entities.CheckableListNote
 import com.amjad.opennote.data.entities.Note
 import com.amjad.opennote.data.entities.NoteType
 import kotlinx.coroutines.runBlocking
 import org.junit.After
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -72,11 +74,9 @@ class RoomDBTest {
     }
 
     @Test
-    @Throws(Exception::class)
     fun addNewNoteTest() {
-
         runBlocking {
-            noteDao.insert(TESTING_NOTE)
+            noteDao.insert(TESTING_NOTE.getNoteObject())
         }
 
 
@@ -85,7 +85,7 @@ class RoomDBTest {
         val note = notes.blockingObserve()?.get(0)
 
 
-        Log.d("BB", "$note")
+        log("$note")
 
         assertEquals(TESTING_NOTE.date, note?.date)
         assertEquals(TESTING_NOTE.title, note?.title)
@@ -101,7 +101,99 @@ class RoomDBTest {
         }
     }
 
+    @Test
+    fun addNewTasksListNote() {
+        runBlocking {
+            noteDao.insert(TESTING_LIST_NOTE.getNoteObject())
+        }
+
+        val notes = noteDao.getAllNotes()
+
+        val note = notes.blockingObserve()?.get(0)?.getNoteBasedOnType()
+
+        log("$note")
+
+        assertEquals(note?.type, NoteType.CHECKABLE_LIST_NOTE)
+        if (note !is CheckableListNote?) {
+            // this is used to enable smart cast below for (taskNote?.noteList)
+            assertTrue(false)
+        } else {
+            assertEquals(
+                TESTING_LIST_NOTE.date,
+                note?.date
+            )
+            assertEquals(TESTING_LIST_NOTE.title, note?.title)
+            assertEquals(TESTING_LIST_NOTE.color, note?.color)
+            assertEquals(
+                TESTING_LIST_NOTE.noteList.map { it.first },
+                note?.noteList?.map { it.first })
+            assertEquals(
+                TESTING_LIST_NOTE.noteList.map { it.second },
+                note?.noteList?.map { it.second })
+        }
+
+        // as its the first note to be inserted
+        assertEquals(1L, note?.id)
+
+        // clear
+        runBlocking {
+            noteDao.deleteAll()
+        }
+    }
+
+    @Test
+    fun addNewTextAndTasksNotes() {
+        runBlocking {
+            noteDao.insert(TESTING_NOTE.getNoteObject())
+            noteDao.insert(TESTING_LIST_NOTE.getNoteObject())
+        }
+
+        val notes = noteDao.getAllNotes().blockingObserve()
+
+        log("$notes")
+
+        assertEquals(2, notes?.size)
+        val textNote =
+            (if (notes?.get(0)?.type == NoteType.TEXT_NOTE) notes[0] else notes?.get(1))?.getNoteBasedOnType()
+        val taskNote =
+            (if (notes?.get(0)?.type == NoteType.CHECKABLE_LIST_NOTE) notes[0] else notes?.get(1))?.getNoteBasedOnType()
+
+        log("$textNote")
+        log("$taskNote")
+
+        assertEquals(TESTING_NOTE.date, textNote?.date)
+        assertEquals(TESTING_NOTE.title, textNote?.title)
+        assertEquals(TESTING_NOTE.note, textNote?.note)
+        assertEquals(TESTING_NOTE.color, textNote?.color)
+
+        if (taskNote !is CheckableListNote?) {
+            // this is used to enable smart cast below for (taskNote?.noteList)
+            assertTrue(false)
+        } else {
+            assertEquals(TESTING_LIST_NOTE.title, taskNote?.title)
+            assertEquals(TESTING_LIST_NOTE.color, taskNote?.color)
+            assertEquals(
+                TESTING_LIST_NOTE.noteList.map { it.first },
+                taskNote?.noteList?.map { it.first })
+            assertEquals(
+                TESTING_LIST_NOTE.noteList.map { it.second },
+                taskNote?.noteList?.map { it.second })
+        }
+
+
+        // clear
+        runBlocking {
+            noteDao.deleteAll()
+        }
+    }
+
     companion object {
+        private const val LOG_TAG = "TESTING"
+
+        fun log(message: String) {
+            Log.d(LOG_TAG, message)
+        }
+
         val TESTING_NOTE = Note(
             NoteType.TEXT_NOTE,
             id = 0,
@@ -109,6 +201,19 @@ class RoomDBTest {
             note = "hello",
             date = Date(1321654),
             color = Color.BLUE
+        )
+
+        val TESTING_LIST_NOTE = CheckableListNote(
+            id = 0,
+            title = "goals",
+            date = Date(321),
+            noteList = listOf(
+                Pair("welcome", true),
+                Pair("another welcome", true),
+                Pair("test note checkable", true),
+                Pair("yes", false),
+                Pair("should work", true)
+            )
         )
     }
 }
