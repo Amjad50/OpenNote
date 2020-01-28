8f2e8afada6d5abd4fc55e410af4421eb521b01b
Amjad Alsharafi <amjadsharafi10@gmail.com>
2020-01-29 00:11:19 +0800

    Add: serialization and deserialization of checkableNoteList
    - transform list into string to store in the DB as note and back

--- app/src/main/java/com/amjad/opennote/data/entities/CheckableListNote.kt
+++ app/src/main/java/com/amjad/opennote/data/entities/CheckableListNote.kt
@@ -29,12 +29,26 @@ class CheckableListNote internal constructor(
     }
 
     private fun populateNoteList(serializedNote: String) {
-        throw NotImplementedError("CheckableListNote::populateNoteList, which is used to convert string note to list")
+        noteList.clear()
+        noteList.addAll(serializedNote.split(SEPARATOR).map {
+            Pair(it.substring(1), it[0] == 'X')
+        })
     }
 
     private fun serializeNoteList(): String {
-        var finalResult = ""
+        // put if its checked or not at the beginning and then proceed with the item text
+        // then join all by the SEPARATOR string
+        return noteList.joinToString(SEPARATOR) {
+            (if (it.second) "X" else "x") + it.first
+        }
+    }
 
-        throw NotImplementedError("CheckableListNote::serializeNoteList, which is used to convert the list to string note")
+    companion object {
+        // this is the BEL character, which is not printable
+        // which means that no (normal) user can type it.
+        // its hackable but the reason I don't care is that it's ok
+        // this is not something big, if its hacked this means that the user can put
+        // separators in the middle on notes which is not something very big of a deal
+        private const val SEPARATOR = (7).toChar().toString()
     }
 }
