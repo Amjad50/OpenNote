34934db4ac397162790ccc52af9dc784acd6f082
Amjad Alsharafi <amjadsharafi10@gmail.com>
2020-02-04 18:03:35 +0800

    Edit: instead of using two viewtypes in list view, combined into one
    - using a LinearLayout that is populated when needed (maybe not best
    option)
    - easier to manage

--- app/src/main/java/com/amjad/opennote/ui/adapters/NotesListAdapter.kt
+++ app/src/main/java/com/amjad/opennote/ui/adapters/NotesListAdapter.kt
@@ -1,8 +1,8 @@
 package com.amjad.opennote.ui.adapters
 
+import android.text.TextUtils
 import android.view.Gravity
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.CheckBox
 import android.widget.TextView
@@ -11,15 +11,15 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.amjad.opennote.R
+import com.amjad.opennote.data.entities.CheckableListNote
 import com.amjad.opennote.data.entities.Note
 import com.amjad.opennote.data.entities.NoteType
-import com.amjad.opennote.databinding.CheckablelistNoteViewBinding
 import com.amjad.opennote.databinding.NoteitemViewBinding
 import com.amjad.opennote.ui.fragments.NotesListFragmentDirections
 import kotlin.math.min
 
 class NotesListAdapter(private val selector: NoteListSelector<Long>) :
-    ListAdapter<Note, NotesListAdapter.BaseNoteViewHolder>(NoteListDiffItemCallBack()) {
+    ListAdapter<Note, NotesListAdapter.NoteViewHolder>(NoteListDiffItemCallBack()) {
 
     init {
         setHasStableIds(true)
@@ -31,54 +31,50 @@ class NotesListAdapter(private val selector: NoteListSelector<Long>) :
 
     override fun getItemId(position: Int): Long = getItem(position).id
 
-    override fun getItemViewType(position: Int): Int {
-        return if (getItem(position).type == NoteType.TEXT_NOTE)
-            VIEWTYPE_TEXTNOTE
-        else
-            VIEWTYPE_LISTNOTE
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseNoteViewHolder {
-        return when (viewType) {
-            VIEWTYPE_TEXTNOTE -> TextNoteViewHolder(
-                NoteitemViewBinding.inflate(
-                    LayoutInflater.from(parent.context),
-                    parent,
-                    false
-                )
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        return NoteViewHolder(
+            NoteitemViewBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
             )
-            VIEWTYPE_LISTNOTE ->
-                CheckableListNoteViewHolder(
-                    CheckablelistNoteViewBinding.inflate(
-                        LayoutInflater.from(parent.context),
-                        parent,
-                        false
-                    )
-                )
-            else -> {
-                throw IllegalArgumentException("Unknown type with code $viewType")
-            }
-        }
+        )
     }
 
-    override fun onBindViewHolder(holder: BaseNoteViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
         holder.bind(getItem(position))
     }
 
-    inner class TextNoteViewHolder(private val binding: NoteitemViewBinding) :
-        BaseNoteViewHolder(binding.root) {
-        override fun bind(note: Note) {
-            // FIXME: this is the same code as below, but cannot move to a method, as it uses two
-            //  different binding classes :'(
+    inner class NoteViewHolder(private val binding: NoteitemViewBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(note: Note) {
             binding.apply {
+
+                // clear views
+                binding.innerNoteContainer.removeAllViews()
+
+                if (note.type == NoteType.CHECKABLE_LIST_NOTE)
+                    bindCheckableListNote(note.getCheckableListNote())
+                else
+                    bindTextNote(note)
+
                 this.note = note
                 selected = selector.isSelected(note.id)
                 setOnNoteClick {
                     if (!selector.hasSelection()) {
                         val action =
-                            NotesListFragmentDirections.actionMainFragmentToNoteEditFragment(
-                                noteId = note.id
-                            )
+                            when (note.type) {
+                                NoteType.TEXT_NOTE ->
+                                    NotesListFragmentDirections.actionMainFragmentToNoteEditFragment(
+                                        noteId = note.id
+                                    )
+                                NoteType.CHECKABLE_LIST_NOTE ->
+                                    NotesListFragmentDirections.actionNoteListFragmentToCheckableListNoteEditFragment(
+                                        noteId = note.id
+                                    )
+                                NoteType.UNDEFINED_TYPE ->
+                                    throw IllegalArgumentException("UNDEFINED_TYPE note, should not exist")
+                            }
 
                         it.findNavController().navigate(action)
                     } else {
@@ -97,30 +93,25 @@ class NotesListAdapter(private val selector: NoteListSelector<Long>) :
                 executePendingBindings()
             }
         }
-    }
 
-    inner class CheckableListNoteViewHolder(private val binding: CheckablelistNoteViewBinding) :
-        BaseNoteViewHolder(binding.root) {
-        override fun bind(note: Note) {
-            binding.apply {
-
-                val notelist = note.getCheckableListNote().noteList
-                notelist.sort()
+        private fun bindCheckableListNote(note: CheckableListNote) {
+            val notelist = note.noteList
+            notelist.sort()
 
 
-                var unchecked = notelist.indexOfFirst { it.isChecked }
-                if (unchecked == -1) // reached to the end but didn't find any
-                    unchecked = notelist.size
+            var unchecked = notelist.indexOfFirst { it.isChecked }
+            if (unchecked == -1) // reached to the end but didn't find any
+                unchecked = notelist.size
 
-                val checked = notelist.size - unchecked
+            val checked = notelist.size - unchecked
 
-                val numberToView = min(unchecked, 5)
-                val numberNotToView = unchecked - numberToView
+            val numberToView = min(unchecked, 5)
+            val numberNotToView = unchecked - numberToView
 
-                listContainer.removeAllViews()
+            binding.innerNoteContainer.run {
                 for (i in 0 until numberToView)
-                    listContainer.addView(
-                        CheckBox(listContainer.context).apply {
+                    addView(
+                        CheckBox(context).apply {
                             text = notelist[i].text
                             setTextColor(0x8a000000.toInt())
                             setButtonDrawable(R.drawable.ic_check_box_unchecked_for_notelist_item)
@@ -132,52 +123,32 @@ class NotesListAdapter(private val selector: NoteListSelector<Long>) :
                     )
 
                 if (numberNotToView > 0)
-                    listContainer.addView(
+                    addView(
                         TextView(binding.root.context).apply {
                             text = context.getString(R.string.checked_items_count, numberNotToView)
                         }
                     )
                 if (checked > 0)
-                    listContainer.addView(
+                    addView(
                         TextView(binding.root.context).apply {
                             text = context.getString(R.string.checked_items_count, checked)
                         }
                     )
-
-                this.note = note
-                selected = selector.isSelected(note.id)
-                setOnNoteClick {
-                    if (!selector.hasSelection()) {
-                        val action =
-                            NotesListFragmentDirections.actionNoteListFragmentToCheckableListNoteEditFragment(
-                                noteId = note.id
-                            )
-
-                        it.findNavController().navigate(action)
-                    } else {
-                        selector.toggle(note.id)
-                    }
-                }
-                binding.noteViewCard.setOnLongClickListener {
-                    if (!selector.hasSelection()) {
-                        selector.select(note.id)
-                    } else {
-                        selector.toggle(note.id)
-                    }
-                    true
-                }
-                executePendingBindings()
             }
         }
-    }
 
-    abstract class BaseNoteViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        abstract fun bind(note: Note)
-    }
 
-    companion object {
-        const val VIEWTYPE_TEXTNOTE = 1
-        const val VIEWTYPE_LISTNOTE = 2
+        private fun bindTextNote(note: Note) {
+            binding.innerNoteContainer.run {
+                addView(
+                    TextView(context).apply {
+                        ellipsize = TextUtils.TruncateAt.END
+                        maxLines = 5
+                        text = note.note
+                    }
+                )
+            }
+        }
     }
 }
 
