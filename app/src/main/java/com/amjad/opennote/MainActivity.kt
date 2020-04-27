58c22d7119d076ec245a4e6d283428b1dff181a9
Amjad Alsharafi <amjadsharafi10@gmail.com>
2020-04-27 22:58:55 +0800

    Edit: used FragmentContainerView for main_activity
    - also removed the usage for binding in main_activity as its pointless.

--- app/src/main/java/com/amjad/opennote/MainActivity.kt
+++ app/src/main/java/com/amjad/opennote/MainActivity.kt
@@ -2,16 +2,18 @@ package com.amjad.opennote
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
-import com.amjad.opennote.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        DataBindingUtil.setContentView<MainActivityBinding>(this, R.layout.main_activity)
+        setContentView(R.layout.main_activity)
+    }
+
+    override fun onStart() {
+        super.onStart()
 
         val navController = findNavController(R.id.nav_host)
         setupActionBarWithNavController(navController)
